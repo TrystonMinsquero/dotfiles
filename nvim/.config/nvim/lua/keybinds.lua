17be4dfbd56e6bd253c47750b5c6be15c0ccd02c
Tryston Minsquero <tryston.minsquero@zynga.com>
2025-12-10 19:34:36 -0600

    Remove unused keybinds for nvim

--- nvim/.config/nvim/lua/keybinds.lua
+++ nvim/.config/nvim/lua/keybinds.lua
@@ -13,15 +13,6 @@ vim.keymap.set("n", "<leader>o", vim.diagnostic.open_float, { desc = "[O]pen flo
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
--- Ctrl+S as god intended
-vim.keymap.set("n", "<C-s>", "<cmd>:w<CR>")
-
-vim.keymap.set("n", "<C-a>", "ggVG", { desc = "Select [A]ll" })
-
--- Replaced by oil.nvim
--- vim.api.nvim_create_user_command("E", "Explore", {})
--- vim.keymap.set("n", "<leader>e", ":Explore<CR>", { desc = "[E]xplore"})
-
 vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "[W]rite" })
 
 -- Vim config
@@ -33,25 +24,11 @@ vim.keymap.set("n", "<leader>vp", ":e ~/.config/nvim/lua/plugins<CR>", { desc = 
 
 vim.keymap.set({ "n", "i", "v" }, "<C-p>", "<C-r>*", { desc = "Paste Default Register" })
 
--- vim.keymap.set("n", "<left>", "<C-w><C-h>", { desc = "Move focus to the left window" })
--- vim.keymap.set("n", "<down>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
--- vim.keymap.set("n", "<up>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
--- vim.keymap.set("n", "<right", "<C-w><C-l>", { desc = "Move focus to the right window" })
---
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
-
 vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>", { desc = "Move to previous item in quick fix list" })
 vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>", { desc = "Move to next item in quick fix list" })
 
 -- Tabbing
-vim.keymap.set({ "n", "v" }, "<leader>tt", function()
-	vim.o.expandtab = not vim.o.expandtab
-end, { desc = "[T]oggle [T]ab", silent = true })
 vim.keymap.set("v", "<leader>r", "<cmd>'<,'>retab!<CR>", { desc = "retab", silent = true })
 
 vim.keymap.set("v", "<S-Tab>", "<gv", { desc = "Indent left", silent = true })
 vim.keymap.set("v", "<Tab>", ">gv", { desc = "Indent right", silent = true })
-
