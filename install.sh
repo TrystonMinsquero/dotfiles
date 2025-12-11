ec4a3c090eedaa401ed68263a1ea5f797435cc61
Tryston Minsquero <trystonminsquero@gmail.com>
2025-12-11 00:19:20 -0600

    update install.sh

--- install.sh
+++ install.sh
@@ -1,48 +1,26 @@
 #!/bin/bash
 
+# run with 
+# curl -fsSL https://raw.githubusercontent.com/TrystonMinsquero/dotfiles/main/install.sh) -o install.sh \
+# && vim install.sh \
+# && ./install.sh \
+# && rm install.sh
+
 # Delete this once you've read the entire script 
 echo "Read through this script so you know what you're installing!"
 exit 1
 
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-## install zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-## install zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
-# Homebrew
-## Install
-echo "Installing Brew..."
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew analytics off
-
-## Formulae
-echo "Installing Brew Formulae..."
-
-
-## Core Utils
-# echo "Install gnu coreutils"
-# brew install coreutils
-
-### Must Have things
-# brew install zsh-autosuggestions
-# brew install zsh-syntax-highlighting
-brew install stow
-# brew install fzf
-brew install bat
-brew install make
-brew install ripgrep
-
-### Terminal
-brew install git
-brew install lazygit
-brew install neovim
-
-## Casks
-
+echo "Installing packages..."
+sudo apt install neovim fzf ripgrep stow lazygit git gh
 
 echo "Installation complete..."
+
+git config --global user.name "Tryston Minsquero"
+git config --global user.email "trystonminsquero@gmail.com"
+git config --global init.defaultBranch "main"
 
 # Clone dotfiles repository
 if [ ! -d "$HOME/dotfiles" ]; then
@@ -55,12 +33,9 @@ cd $HOME/dotfiles || exit
 
 # Stow dotfiles packages
 echo "Stowing dotfiles..."
-stow -t ~ *
+stow nvim zshrc
 
 source ~/.zshrc
 
 echo "Dotfiles setup complete!"
 
-git config --global user.name "Tryston Minsquero"
-git config --global user.email "trystonminsquero@gmail.com"
-git config --global init.defaultBranch "main"
