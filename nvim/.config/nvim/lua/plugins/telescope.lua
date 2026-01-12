3a65458b47f67db88a8cc7cee2ed663c8ca7f383
Tryston Minsquero <trystonminsquero@gmail.com>
2026-01-12 00:00:52 -0600

    nvim: switch around grep hotkey

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -70,12 +70,12 @@ return {
 			vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
 			vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
 			vim.keymap.set("n", "<leader>sq", builtin.quickfix, { desc = "[S]earch [Q]uickfix" })
-			vim.keymap.set("n", "<leader>sg", function()
+			vim.keymap.set("n", "<leader>sG", function()
 				builtin.live_grep({
 					cwd = require("telescope.utils").buffer_dir(),
 				})
 			end, { desc = "[S]earch by [G]rep from current directory" })
-			vim.keymap.set("n", "<leader>sG", builtin.live_grep, { desc = "[S]earch by [G]rep from working directory" })
+			vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep from working directory" })
 			vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
 			vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
 			-- vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
