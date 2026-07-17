2f11c866fc1b6eaada809fa9c1b0b729376c42c1
Tryston Minsquero <tryston.minsquero@zynga.com>
2026-07-17 09:39:48 -0500

    Add fff

---
+++ nvim/.config/nvim/lua/plugins/fff.lua
@@ -0,0 +1,50 @@
+return {
+	{
+		-- https://github.com/dmtrKovalenko/fff#fffnvim
+		"dmtrKovalenko/fff.nvim",
+		build = function()
+			-- downloads a prebuilt binary or falls back to cargo build
+			require("fff.download").download_or_build_binary()
+		end,
+		-- for nixos:
+		-- build = "nix run .#release",
+		opts = {
+			debug = {
+				enabled = true,
+				show_scores = true,
+			},
+		},
+		lazy = false, -- the plugin lazy-initialises itself
+		keys = {
+			{
+				"<leader>ff",
+				function()
+					require("fff").find_files()
+				end,
+				desc = "FFFind files",
+			},
+			{
+				"<leader>fg",
+				function()
+					require("fff").live_grep()
+				end,
+				desc = "LiFFFe grep",
+			},
+			{
+				"<leader>fz",
+				function()
+					require("fff").live_grep({ grep = { modes = { "fuzzy", "plain" } } })
+				end,
+				desc = "Live fffuzy grep",
+			},
+			{
+				"<leader>fw",
+				function()
+					require("fff").live_grep_under_cursor()
+				end,
+				mode = { "n", "x" },
+				desc = "Search current word / selection",
+			},
+		},
+	},
+}
